589361730e64eb8a89639441124f0432ff7f35bb
The-Bug-Developer <122955186+The-Bug-Developer@users.noreply.github.com>
2023-01-24 10:04:16 -0800

    Add files via upload

---
+++ lab0.py
@@ -0,0 +1,15 @@
+#import numpy
+#import matplotlib.pyplot
+#import scipy
+#import control
+#import pandas
+
+################################################################
+#                                                              #
+# Zachary DeLuca                                               #
+# ECE 351 Section 53                                           #
+# Lab 00                                                       #
+# Due: Jan 24                                                  #
+#                                                              #
+################################################################
+
